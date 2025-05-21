47650223d9f2ff300ce82eb9d5c54b967a4432b4
saiabhi1908 <praveenakolli08@gmail.com>
2025-05-22 05:27:41 +0530

    three

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,10 +37,9 @@ const Navbar = () => {
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/admin' >
-  <li className='py-1'>ADMIN PANEL</li>
-  <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
-</NavLink>
-
+          <li className='py-1'>ADMIN PANEL</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+        </NavLink>
       </ul>
 
       <div className='flex items-center gap-4 '>
@@ -53,6 +52,7 @@ const Navbar = () => {
                 <div className='min-w-48 bg-gray-50 rounded flex flex-col gap-4 p-4'>
                   <p onClick={() => navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
                   <p onClick={() => navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                  <p onClick={() => navigate('/setup-2fa')} className='hover:text-black cursor-pointer'>Setup 2FA</p>
                   <p onClick={logout} className='hover:text-black cursor-pointer'>Logout</p>
                 </div>
               </div>
@@ -73,7 +73,6 @@ const Navbar = () => {
             <NavLink onClick={() => setShowMenu(false)} to='/about' ><p className='px-4 py-2 rounded full inline-block'>ABOUT</p></NavLink>
             <NavLink onClick={() => setShowMenu(false)} to='/contact' ><p className='px-4 py-2 rounded full inline-block'>CONTACT</p></NavLink>
             <NavLink onClick={() => setShowMenu(false)} to='/admin'><p className='px-4 py-2 rounded full inline-block'>ADMIN PANEL</p></NavLink>
-
           </ul>
         </div>
       </div>
